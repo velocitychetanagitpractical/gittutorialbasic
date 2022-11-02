2784b8a431eea8c7ef787c06f1463dc39ec75ebf
velocitychetanagitpractical <117296444+velocitychetanagitpractical@users.noreply.github.com>
2022-11-02 21:29:59 +0530

    Add files via upload

---
+++ Amazon.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Amazon {
+	
+	public static void main(String[] args) {
+		Transaction t = new Transaction();
+		t.withdraw(10000);
+		t.display();
+	}
+
+}
---
+++ Transaction.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Transaction {
+	
+	void withdraw(int amtToWithdraw) {
+		System.out.println("end user imp info");
+	}
+	
+	void display() {
+		System.out.println("implementation logic");
+	}
+
+}
